5c4baaa0364bc979532d3244ace688928b32ae45
santiago-franco03 <401101@sistemas.frc.utn.edu.ar>
2026-04-18 15:08:43 -0300

    tienda anashe

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme.dart';
-import 'screens/home_screen.dart';
+import 'screens/tienda_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
       title: '4 Cartas BLITZ',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.build(),
-      home: const HomeScreen(),
+      home: const TiendaScreen(),
     );
   }
 }
